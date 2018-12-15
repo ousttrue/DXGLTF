814084e445b0929852bd0f220d63c515cf1817e9
ousttrue <ousttrue@gmail.com>
2018-12-15 15:21:09 +0900

    File dialog

--- DXGLTF/Form1.Designer.cs
+++ DXGLTF/Form1.Designer.cs
@@ -67,7 +67,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "DXGLTF";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
--- DXGLTF/Form1.cs
+++ DXGLTF/Form1.cs
@@ -17,9 +17,46 @@ namespace DXGLTF
             InitializeComponent();
         }
 
+        struct FileDialogFilter
+        {
+            public string Label;
+            public string[] Extensions;
+            public FileDialogFilter(string label, params string[] extensions)
+            {
+                Label = label;
+                Extensions = extensions;
+            }
+            public override string ToString()
+            {
+                var joined = string.Join(";", Extensions.Select(x => "*." + x));
+                return String.Format("{0}({1})|{2}", Label, joined, joined);
+            }
+        }
+        static FileDialogFilter[] filters = new FileDialogFilter[]
+        {
+            new FileDialogFilter("glTF files", new string[]{"gltf","glb"}),
+            new FileDialogFilter("All files", new string[]{"*"}),
+        };
+
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                //openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = string.Join("|", filters.Select(x => x.ToString()));
+                openFileDialog.FilterIndex = 0;
+                //openFileDialog.RestoreDirectory = true;
+                if (openFileDialog.ShowDialog() != DialogResult.OK)
+                {
+                    return;
+                }
+                OpenFile(openFileDialog.FileName);
+            }
+        }
 
+        void OpenFile(string file)
+        {
+            Console.WriteLine(file);
         }
     }
 }
