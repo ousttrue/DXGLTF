e54000bf8d185e047fc2c3333c88a80b0089b6a1
ousttrue <ousttrue@gmail.com>
2018-12-21 04:15:34 +0900

    object tracking

--- DXGLTF/Program.cs
+++ DXGLTF/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace DXGLTF
 {
@@ -14,6 +12,7 @@ namespace DXGLTF
         [STAThread]
         static void Main()
         {
+            SharpDX.Configuration.EnableObjectTracking = true;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
