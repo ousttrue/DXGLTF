5c341164eaf72ed40732d235f50e11ec50283dab
ousttrue <ousttrue@gmail.com>
2019-03-03 03:05:37 +0900

    TRS

--- DXGLTF/Assets/Node.cs
+++ DXGLTF/Assets/Node.cs
@@ -51,6 +51,7 @@ namespace DXGLTF.Assets
             _children.Clear();
         }
 
+        #region Local
         Matrix _localMatrix = Matrix.Identity;
         public Matrix LocalMatrix
         {
@@ -58,8 +59,91 @@ namespace DXGLTF.Assets
             set
             {
                 _localMatrix = value;
+                Quaternion q;
+                _localMatrix.Decompose(out _localScale, out q, out _localPosition);
+                _localEuler = ToEulerAngle(q);
             }
         }
+
+        static double CopySign(double a, double b)
+        {
+            if (b < 0)
+            {
+                return -Math.Abs(a);
+            }
+            else
+            {
+                return Math.Abs(a);
+            }
+        }
+
+        /// <summary>
+        /// https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+        /// </summary>
+        /// <param name="q"></param>
+        /// <returns></returns>
+        static Vector3 ToEulerAngle(Quaternion q)
+        {
+            // roll (x-axis rotation)
+            var sinr_cosp = +2.0 * (q.W * q.X + q.Y * q.Z);
+            var cosr_cosp = +1.0 - 2.0 * (q.X * q.X + q.Y * q.Y);
+            var roll = Math.Atan2(sinr_cosp, cosr_cosp);
+
+            // pitch (y-axis rotation)
+            var sinp = +2.0 * (q.W * q.Y - q.Z * q.X);
+            var pitch = (Math.Abs(sinp) >= 1)
+                ? CopySign(Math.PI / 2, sinp) // use 90 degrees if out of range
+                : Math.Asin(sinp)
+                ;
+
+            // yaw (z-axis rotation)
+            double siny_cosp = +2.0 * (q.W * q.Z + q.X * q.Y);
+            double cosy_cosp = +1.0 - 2.0 * (q.Y * q.Y + q.Z * q.Z);
+            var yaw = Math.Atan2(siny_cosp, cosy_cosp);
+
+            return new Vector3((float)roll, (float)pitch, (float)yaw);
+        }
+
+        void CalcMatrix()
+        {
+            _localMatrix = Matrix.Transformation(Vector3.Zero, Quaternion.Identity, _localScale,
+                Vector3.Zero, Quaternion.RotationYawPitchRoll(_localEuler.Y, _localEuler.X, _localEuler.Z),
+                _localPosition);
+        }
+
+        Vector3 _localPosition;
+        public Vector3 LocalPosition
+        {
+            get { return _localPosition; }
+            set
+            {
+                _localPosition = value;
+                CalcMatrix();
+            }
+        }
+
+        Vector3 _localEuler;
+        public Vector3 LocalEuler
+        {
+            get { return _localEuler; }
+            set
+            {
+                _localEuler = value;
+                CalcMatrix();
+            }
+        }
+
+        Vector3 _localScale;
+        public Vector3 LocalScale
+        {
+            get { return _localScale; }
+            set
+            {
+                _localScale = value;
+                CalcMatrix();
+            }
+        }
+        #endregion
 
         Matrix _worldMatrix = Matrix.Identity;
         public Matrix WorldMatrix
--- DXGLTF/Controls/SelectedNodeContent.cs
+++ DXGLTF/Controls/SelectedNodeContent.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DXGLTF.Assets;
+using SharpDX;
+using System;
 using System.Reactive.Linq;
 using System.Threading;
 using WeifenLuo.WinFormsUI.Docking;
@@ -14,10 +16,48 @@ namespace DXGLTF
 
             hierarchy.SelectedObservable
                 .ObserveOn(SynchronizationContext.Current)
-                .Subscribe(x =>
-                {
-                    propertyGrid1.SelectedObject = x;
-                });
+                .Subscribe(SetNode)
+                ;
+        }
+
+        class NodeProps
+        {
+            Node _node;
+
+            public Vector3 T
+            {
+                get { return _node.LocalPosition; }
+                set { _node.LocalPosition = value; }
+            }
+
+            public Vector3 R
+            {
+                get { return _node.LocalEuler; }
+                set { _node.LocalEuler = value; }
+            }
+
+            public Vector3 S
+            {
+                get { return _node.LocalScale; }
+                set { _node.LocalScale = value; }
+            }
+
+            public NodeProps(Node node)
+            {
+                _node = node;
+            }
+        }
+
+        void SetNode(Node node)
+        {
+            if (node == null)
+            {
+                propertyGrid1.SelectedObject = null;
+            }
+            else
+            {
+                propertyGrid1.SelectedObject = new NodeProps(node);
+            }
         }
     }
 }
