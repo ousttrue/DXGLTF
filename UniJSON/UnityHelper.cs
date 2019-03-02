1055d7b4763db6d7b68e5119bd4d64cdffdf9058
ousttrue <ousttrue@gmail.com>
2019-03-02 20:40:40 +0900

    Restore UniJSON

---
+++ UniJSON/UnityHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace UnityEngine
+{
+    [Serializable]
+    public struct Vector2
+    {
+        public float x;
+        public float y;
+    }
+
+    [Serializable]
+    public struct Vector3
+    {
+        public float x;
+        public float y;
+        public float z;
+    }
+
+    [Serializable]
+    public struct Vector4
+    {
+        public float x;
+        public float y;
+        public float z;
+        public float w;
+    }
+}
